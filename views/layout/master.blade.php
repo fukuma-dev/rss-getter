50a5f10e3628a87e8d2bf09cfac099842d7dd999
Ryutaro Fukuma <ricken0203@gmail.com>
2020-06-01 16:53:29 +0900

    fixed: style

--- views/layout/master.blade.php
+++ views/layout/master.blade.php
@@ -3,19 +3,8 @@
 <head>
     <title>FC2 RSS 記事検索</title>
 </head>
-<body>
+<body style="font-family:Helvetica Neue, Arial, Hiragino Kaku Gothic ProN, Hiragino Sans, Meiryo, sans-serif; background-color: #f4f5f7;">
 @yield('header')
 @yield('content')
 </body>
-<style>
-    body {
-        font-family: "Helvetica Neue",
-        Arial,
-        "Hiragino Kaku Gothic ProN",
-        "Hiragino Sans",
-        Meiryo,
-        sans-serif;
-        background-color: #f4f5f7;
-    }
-</style>
 </html>
